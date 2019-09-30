751f579ad245b869396066b01be0cc4aa34b3a64
Scott Davis <stdavis@utah.gov>
2019-09-30 10:23:54 -0600

    chore: modernize nhd stream data import script

--- scripts/createStreamData.py
+++ scripts/createStreamData.py
@@ -1,78 +1,82 @@
 import arcpy
+import sys
+from os.path import join
 
-sdeStream = r'C:\inetpub\wwwroot\serverprojects\MapData\SGID10.gdb\StreamsNHDHighRes'
-waterQualityFGDB = r'C:\inetpub\wwwroot\serverprojects\MapData\SurfaceWaterQuality\Data_1.gdb'
-temp = waterQualityFGDB + '\\temp'
-temp2 = waterQualityFGDB + '\\temp2'
-temp3 = waterQualityFGDB + '\\temp3'
-streamsLocal = waterQualityFGDB + '\\StreamsDissolved'
+
+sde = sys.argv[1]
+sdeStream = join(sde, 'SGID10.WATER.StreamsNHDHighRes')
+waterQualityFGDB = sys.argv[2]
+temp = join(waterQualityFGDB, 'temp')
+temp2 = join(waterQualityFGDB, 'temp2')
+temp3 = join(waterQualityFGDB, 'temp3')
+streamsLocal = join(waterQualityFGDB, 'StreamsDissolved')
 GNIS_Name = 'GNIS_Name'
 COUNTY = 'COUNTY'
 NAME = 'NAME'
-counties = r'Database Connections\SGID10_DirectConnect.sde\SGID10.BOUNDARIES.Counties'
+counties = join(sde, 'SGID10.BOUNDARIES.Counties')
 dissolveTempField = 'tempDissolve'
 
 if arcpy.Exists(streamsLocal):
-    print 'deleting old streams local data'
+    print('deleting old streams local data')
     arcpy.Delete_management(streamsLocal)
-    print arcpy.GetMessages()
+    print(arcpy.GetMessages())
 
-print '\n\ncreating layer'
+print('creating layer')
 lyr = arcpy.MakeFeatureLayer_management(sdeStream, 'streamLayer', "\"%s\" is not null AND \"%s\" <> ''" % (GNIS_Name, GNIS_Name))
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\n' + str(arcpy.GetCount_management(lyr))
+print('' + str(arcpy.GetCount_management(lyr)))
 
-print '\n\ndissolving'
+print('dissolving')
 arcpy.Dissolve_management(lyr, temp, GNIS_Name)
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\nExploding lines'
+print('Exploding lines')
 arcpy.MultipartToSinglepart_management(temp, temp2)
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\nidentity'
+print('identity')
 arcpy.Identity_analysis(temp2, counties, temp3)
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\nadding dissolve field'
+print('adding dissolve field')
 arcpy.AddField_management(temp3, dissolveTempField, "TEXT", '', '', 50)
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\ncalculating dissolve field'
+print('calculating dissolve field')
 arcpy.CalculateField_management(temp3, dissolveTempField, '!%s! + !%s!' % (GNIS_Name, NAME), "PYTHON")
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\ncreating layer to filer outside of state'
+print('creating layer to filer outside of state')
 lyr2 = arcpy.MakeFeatureLayer_management(temp3, 'tempLayer', "\"%s\" <> ''" % (NAME))
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\ndissolving again'
+print('dissolving again')
 arcpy.Dissolve_management(lyr2, streamsLocal, dissolveTempField)
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\njoining fields'
+print('joining fields')
 arcpy.JoinField_management(streamsLocal, dissolveTempField, temp3, dissolveTempField, '%s;%s' % (GNIS_Name, NAME))
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\nadding ' + COUNTY
+print('adding ' + COUNTY)
 arcpy.AddField_management(streamsLocal, COUNTY, 'TEXT', '', '', 25)
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\ncalculating ' + COUNTY
+print('calculating ' + COUNTY)
 arcpy.CalculateField_management(streamsLocal, COUNTY, '!%s!' % (NAME), 'PYTHON')
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\ndeleting ' + NAME
+print('deleting ' + NAME)
 arcpy.DeleteField_management(streamsLocal, NAME)
-print arcpy.GetMessages()
+print(arcpy.GetMessages())
 
-print '\n\ndelete'
-#arcpy.Delete_management(temp)
-#print arcpy.GetMessages()
-#arcpy.Delete_management(temp2)
-#print arcpy.GetMessages()
-#arcpy.Delete_management(temp3)
-#print arcpy.GetMessages()
+print('delete')
+arcpy.Delete_management(temp)
+print(arcpy.GetMessages())
+arcpy.Delete_management(temp2)
+print(arcpy.GetMessages())
+arcpy.Delete_management(temp3)
+print(arcpy.GetMessages())
 
-print 'done'
+print('done')
