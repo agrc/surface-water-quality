8b8930adc5752b2a1d70fdbbe1ccaf9c0f515879
stdavis <stdavis@utah.gov>
2024-10-04 11:28:01 -0600

    ci: move deploy jobs to separate release workflow

--- src/app/config.js
+++ src/app/config.js
@@ -12,7 +12,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '1.3.6',
+        version: '1.3.6', // x-release-please-version
 
         fields: {
             streams: {
